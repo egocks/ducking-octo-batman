c8e623e0744826e5348dbd1f9c843f9c8f8e3c17
Eliel Goco <egocks@ymail.com>
2015-05-15 20:55:38 +0800

    added comments, print statements, delay during stop

--- motor_switch.py
+++ motor_switch.py
@@ -6,7 +6,13 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+# PIR config:
+# red to 5v, black to GND, yellow to #18
 pir_pin = 18
+
+# stepper motor config: see adafruit tutorial
+# https://learn.adafruit.com/adafruits-raspberry-pi-lesson-10-stepper-motors
+# I used a ULN2803 chip, so #18 was free for use by the PIR as input
 coil_A_1_pin = 4
 coil_A_2_pin = 17
 coil_B_1_pin = 23
@@ -47,11 +53,14 @@ def setStep(w1, w2, w3, w4):
 	GPIO.output(coil_B_2_pin, w4)
 
 moved = False
-forwarded = False
 while True:
 	if GPIO.input(pir_pin):
 		moved = not moved
+		print("movement toggled " + moved)
 	if moved:
 		forward(int(2) / 1000.0, int(100))	
+		print("moved forward")
 	else:
 		setStep(0,0,0,0)
+		print("stopped")
+		time.sleep(0.1)
